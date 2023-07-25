9fd26497484f035d7d510a099200406294c31c9f
Zong Han Chin <chinzonghan@beaches.app>
2023-07-25 00:33:20 -0700

    finished a simple working wrapper around mongo_dart

--- example/mongo_example/app.dart
+++ example/mongo_example/app.dart
@@ -1,8 +1,8 @@
 import 'package:dartive/dartive.dart';
-import 'package:dartive/dartiveMongo.dart';
+// import 'package:dartive/dartiveMongo.dart';
+import '../../lib/dartiveMongo.dart';
 import 'dart:io' show Platform;
 
-import 'package:mongo_dart/mongo_dart.dart';
 
 String host = Platform.environment['MONGO_DART_DRIVER_HOST'] ?? '0.0.0.0';
 String port = Platform.environment['MONGO_DART_DRIVER_PORT'] ?? '27017';
@@ -10,22 +10,26 @@ String port = Platform.environment['MONGO_DART_DRIVER_PORT'] ?? '27017';
 void main(List<String> arguments) async {
 
   var uri = 'mongodb://$host:$port/mongo_dart-blog';
-
   /// Create a client
   var client = await DartiveMongo.client(uri);
   /// connect
+  await client.connect();
+  // const database - client.db('beaches')
+  var collection = client.collection('beaches');
 
-  await client.open();
+
+
 
   /// find
-  DartiveMongo.find({'name': 'Tom', 'rating': {r'$gt': 10}}, "name, friends", () async {
-    return {
-      "name": "Tom",
-      "friends": {"John", "Jane"}
-    };
-  });
+  // var results = await collection.find({'name': 'Tom', 'rating': {r'$gt': 10}}, "name, friends", () async {
+  //   return {
+  //     "name": "Tom",
+  //     "friends": {"John", "Jane"}
+  //   };
+  // });
 
   /// findOne
+  var result = await collection.findOne({'name': 'Tom', 'rating': {r'$gt': 10}});
   /// insertMany
   /// insertAll
   /// insertOne
--- lib/dartiveMongo.dart
+++ lib/dartiveMongo.dart
@@ -4,43 +4,69 @@ import 'package:dartive/dartive.dart';
 
 
 
+
 class DartiveMongo {
-  /// Private constructor to create a DartiveMongo Client around mongo_dart's Db class
-  /// TODO add Dartive logger for debugging
-  // final Db _db;
+  /// Private constructor to create a DartiveMongo wrapper around mongo_dart's Db class
+  final Db _db;
   // DartiveMongo(String uri): _db = Db(uri);
+  DartiveMongo._(this._db);
   
 
-  late Future<Db> _db;
-
-  /// Creates a mongoDB client and is able to handle connection for both standard format and DNS seedlist (SRV) format.
-  DartiveMongo(String uri) {
-    _db = Db.create(uri);
+  /// Public factory constructor that <br>
+  /// creates a mongoDB client and is able to handle connection for both standard format and DNS seedlist (SRV) format.
+  static Future<DartiveMongo> client(String uriString, [bool useMongoDart = false]) async {
+    try {
+      final db = await Db.create(uriString);
+      return DartiveMongo._(db);
+    } catch (e) {
+      throw Dartive.logger('Error creating connection: $e');
+    }
   }
 
-  Future<void> open() async => await (await _db).open();
-  
 
-  
-  static Future<Db> client(String uriString, [bool useMongoDart = false]) async {
-    if (useMongoDart) {
-      try {
-        return Db.create(uriString);
-      } catch (e) {
-        throw Dartive.logger('Error creating connection: $e');
-      }
-    } else throw Dartive.logger('TODO');
-    
+
+  /// Drop collection to clear the database
+  Future<bool> dropCollection(String collectionName) async => await _db.dropCollection(collectionName);
+
+  /// Connects to the mongoDB using open
+  Future<void> connect() async => await _db.open();
+
+  /// Finds the mongoDB collection specified
+  DartiveCollection collection(String collectionName) {
+    return DartiveCollection(_db, collectionName);
   }
   
-
-  
-
-  // await db.open();
-
-
-  static void find(query, projection, [options]) => null;
 
 
   Future<void> close() async => await (await _db).close();
+}
+
+
+class DartiveCollection {
+
+  late DbCollection _collection;
+
+  DartiveCollection(Db _db, String collectionName) {
+    _collection = _db.collection(collectionName);
+  }
+
+
+  Future<Map<String, dynamic>?> findOne([selector]) {
+    return _collection.findOne(selector);
+  }
+
+  Future<Map<String, dynamic>> remove(selector, {WriteConcern? writeConcern}) {
+    return _collection.remove(selector, writeConcern: writeConcern);
+  }
+
+  Future<Map<String, dynamic>> insertOne(Map<String, dynamic> document) async {
+    return await _collection.insert(document);
+  }
+
+  Future<BulkWriteResult> insertMany(List<Map<String, dynamic>> documents) async {
+    return await _collection.insertMany(documents);
+  }
+
+
+  static void find(query, projection, [options]) => null;
 }
